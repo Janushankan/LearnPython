86d766cbafeb91f9a1bd86c13386f5a4dd4aba40
Janushankan <vj.janushankan1006@gmail.com>
2023-12-03 15:54:50 +0530

    changes

---
+++ app1.py
@@ -0,0 +1,36 @@
+# if statements -------------------------------------
+
+# is_hot = True
+# is_hot = False
+
+# if is_hot:
+#     print("It's a hot day")
+#     print("Drink plenty of water")
+# else:
+#     print("It's a cold day")
+#     print("Wear warm clothes")
+# print("Enjoy your day")
+
+
+# is_hot = False
+# is_cold = True
+# # is_hot = False
+
+# if is_hot:
+#     print("It's a hot day")
+#     print("Drink plenty of water")
+# elif is_cold:
+#     print("It's a cold day")
+#     print("Wear warm clothes")
+# else:
+#     print("It's a lovely day")
+
+# print("Enjoy your day")
+
+price = 1000000
+has_good_credit = True
+if has_good_credit:
+    down_payment = 0.1 * price
+else:
+    down_payment = 0.2 * price
+print(f"Down Payment: ${down_payment}")
